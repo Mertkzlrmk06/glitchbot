5cc88fb3077bf3df86d248a280f70030b6e090c2
Mertkzlrmk06 <mertkzlrmk@yandex.ru>
2019-08-16 03:56:07 +0300

    Initial commit

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('https://media.giphy.com/media/eSQFIjNhmeX28/giphy.gif Hoşgeldin  **' + username + '**  https://i.pinimg.com/originals/af/d9/f6/afd9f6f1c984d8f54412cb9de9607465.gif :wink:');
+
+};
